765e425bbfc23c0db04adae1f50666204b3acf6a
theylovecjawn <cjohn@brooklynsteamcenter.com>
2024-02-01 10:57:35 -0500

    signup made

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
+
+conn = pymysql.connect(
+    host='10.100.33.60',
+    user='cjohn',
+    password='224257683',
+    database='cjohn_dynasty',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 
 @app.route('/')
 def index():
     return render_template('landing.html.jinja')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        fname = request.form['first_name']
+        lname = request.form['last_name']
+        dob = request.form['birthday']
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = conn.cursor()
+
+        # Using parameterized query to prevent SQL injection
+        sql = "INSERT INTO users (fname, lname, dob, username, password) VALUES (%s, %s, %s, %s, %s)"
+
+        
+
+    return render_template('signup.html')
+
+
