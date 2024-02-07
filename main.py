73c4740f887813d8d95ee5a3cda4f70c45f5ff54
theylovecjawn <cjohn@brooklynsteamcenter.com>
2024-02-07 10:49:57 -0500

    edits

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
+import flask_login
 import pymysql
 import pymysql.cursors
 
 app = Flask(__name__)
+
+app.secret_key = "thisisthesupersecretpassworddonttellnobody"
+
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+    def __init__(self, id, username, pfp):
+        self.id = id
+        self.username = username
+        self.pfp = pfp
+    def get_id(self):
+        return str(self.id)
+    
+
+        
+
 
 conn = pymysql.connect(
     host='10.100.33.60',
@@ -12,6 +34,18 @@ conn = pymysql.connect(
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor
 )
+
+@login_manager.user_loader
+
+def load_user(user_id):
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM `users` WHERE `id` = ' + str(user_id))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.commit()
+    if result is None:
+        return None
+    return User(result["id"],result["pfp"],result["username"])
 
 @app.route('/')
 def index():
@@ -33,6 +67,7 @@ def signup():
         cursor.execute(f"INSERT INTO `users` (fname, lname, dob, username, password) VALUES ('{fname}', '{lname}', '{dob}', '{username}', '{password}')")
         cursor.close()
         conn.commit()
+
         
         return redirect(url_for('login'))
 
@@ -47,17 +82,20 @@ def login():
         password = request.form['password']
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM `users` WHERE username='{username}'")
-        user = cursor.fetchone()
-        if user and user['password'] == password:
+        result = cursor.fetchone()
+        if result and result['password'] == password:
+            user = load_user(result["id"])
+            flask_login.login_user(user)
             return redirect('/feed')
         else:
             error= "Invalid username or password. Please try again."
             return render_template('login.html', error=error)
-
     return render_template('login.html')
 
+
 @app.route('/feed')
-def feed():
+@flask_login.login_required
+def post_feed():
     return "Hello World"
 
 
